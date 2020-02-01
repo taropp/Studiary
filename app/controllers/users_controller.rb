9e73ac8eed476b0649ad444ee75497ad3d0eb3c9
EC2 Default User <ec2-user@ip-172-31-42-161.ap-northeast-1.compute.internal>
2020-02-01 08:59:06 +0000

    ページネーションの表示に関する変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
-  PER = 10
-  
   def index
-    @users = User.page(params[:page]).per(PER)
+    @users = User.page(params[:page]).per(10)
   end
   def show
     @user = User.find(params[:id])
