3e6fef3dc2134c980006b913013ad04a20caaf4c
EC2 Default User <ec2-user@ip-172-31-42-161.ap-northeast-1.compute.internal>
2020-02-02 07:22:34 +0000

    投稿の削除機能追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,5 @@
 class MicropostsController < ApplicationController
+  before_action :correct_user, only: :destroy
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -10,9 +11,16 @@ class MicropostsController < ApplicationController
     end
   end
   def destroy
+    @micropost.destroy
+    flash[:notice] = "削除しました"
+    redirect_back(fallback_location: root_path)
   end
   private
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_path if @micropost.nil?
     end
 end
