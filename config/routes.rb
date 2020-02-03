8aa9724a2dfb0dff6345c2203f28c35b54ef3926
EC2 Default User <ec2-user@ip-172-31-42-161.ap-northeast-1.compute.internal>
2020-02-03 12:48:12 +0000

    facebook認証のAPIが表に出ていたため変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'pages#home'
   resources :users do
     member do
