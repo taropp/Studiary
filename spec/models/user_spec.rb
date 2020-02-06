b46abf5e3cab02be42bcb1adc999a507bc090391
Takumi_Kobayashi <taropp89728972@gmail.com>
2020-02-06 01:02:29 +0000

    テストの記入,それに伴う必要なgemの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with a name, email, and password" do
+    user = User.new(
+      name: "ルギア",
+      email: "test@example.com",
+      password: "hogehoge"
+      )
+      expect(user).to be_valid
+    end
+  
+  it "is invalid without a name" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("を入力してください")
+  end
+  
+  it "is invalid without an email address" do
+    User.create(
+      name: "ルギア",
+      email: "test@example.com",
+      password: "hogehoge"
+      )
+      
+    user = User.new(
+      name: "ルギア",
+      email: "test@example.com",
+      password: "hogehoge"
+      )
+      
+    user.valid?
+    expect(user.errors[:email]).to include("はすでに存在します")
+  end
+  
+  it "is invalid with a duplicate email address"
+  
+  it "returns a user's full name as a string"
+  
+end
