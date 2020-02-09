33483c9002399c33e32852d562246ffbbf6bdc88
Takumi_Kobayashi <taropp89728972@gmail.com>
2020-02-09 23:55:21 +0000

    検索機能が作動していなかった原因(テスト書いている最中に書き換えていた)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
+  belongs_to :user
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
