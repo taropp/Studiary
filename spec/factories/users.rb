6cd5555821be525ebd57a2e0cdfeeb61321bb526
Takumi_Kobayashi <taropp89728972@gmail.com>
2020-02-09 05:27:17 +0000

    ルボコップ警察

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name {"ピカチュウ"}
-    sequence(:email) { |n| "satoshi_kirai#{n}@example.com"}
-    password {"pippikachu"}
+    name { "ピカチュウ" }
+    sequence(:email) { |n| "satoshi_kirai#{n}@example.com" }
+    password { "pippikachu" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   it "has a valid factory" do
     expect(FactoryBot.build(:user)).to be_valid
   end
-  
+
   it "is valid with a name, email, and password" do
     user = User.new(
       name: "ルギア",
@@ -13,13 +13,13 @@ RSpec.describe User, type: :model do
     )
     expect(user).to be_valid
   end
-  
+
   it "is invalid without a name" do
     user = FactoryBot.build(:user, name: nil)
     user.valid?
     expect(user.errors[:name]).to include("を入力してください")
   end
-  
+
   it "is invalid without a name" do
     user = User.new(name: nil)
     user.valid?
@@ -42,5 +42,4 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:email]).to include("はすでに存在します")
   end
-
 end
